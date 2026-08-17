c37fb7955150e3c78f401ceeda8365460243e4d0
Miguel Guerrero <info@masmercat.com>
2026-08-17 08:59:07 +0200

    chore: secure env and fix migration tooling

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,4 +1,5 @@
-import { DataSource, DataSourceOptions } from 'typeorm';
+import { DataSource } from 'typeorm';
+import type { DataSourceOptions } from 'typeorm';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
